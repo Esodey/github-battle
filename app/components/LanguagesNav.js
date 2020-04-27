07680539d1073eb03861939631131c61c2e3718c
Erik Soderholm <esodey@gmail.com>
2020-04-27 18:43:06 -0500

    "Install proptypes and use them in the languagesNav component"

--- app/components/LanguagesNav.js
+++ app/components/LanguagesNav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function LanguagesNav ({ selected, onUpdateLanguage}) {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -17,4 +18,10 @@ export default function LanguagesNav ({ selected, onUpdateLanguage}) {
         ))}
       </ul>
     )
+}
+
+LanguagesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired
+
 }
